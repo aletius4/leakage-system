9003e4b0f9e28635adcac5f51719971e447e4f84
Aletius Venance <aletius02@gmail.com>
2025-06-26 21:36:00 -0700

    fix timezone setting

--- Agro/settings.py
+++ Agro/settings.py
@@ -95,7 +95,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'Africa/Dar_es_salaam'
+TIME_ZONE = 'Africa/Dar_es_Salaam'
 USE_I18N = True
 USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
